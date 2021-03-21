68245e626ba357864a907dc57658b67881550355
Hristian Bozinoski <konten.bozinoski@gmail.com>
2021-03-21 11:08:59 +0100

    Angular experiment beginning

---
+++ Experiment/src/app/square/square.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SquareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
